870d26181128c9e6734980341a9695cba6502b29
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-07-28 11:21:11 -0600

    Update script.js
    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ const btnT = document.querySelector('.btn');
 btnT.addEventListener('click', () => { document.getElementById('menu').classList.toggle('active'); });
 const btn2 = document.querySelector('.menu');
 btn2.addEventListener('click', () => { document.getElementById('menu').classList.toggle('active'); });
+
+// Data store
+
